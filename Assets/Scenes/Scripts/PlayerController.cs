0cdb3d1535f1829602876c82167ce98db28c60a4
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2018-12-18 00:13:52 -0600

    edited player controller script to display text and collectable count

--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -28,6 +34,17 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 6)
+        {
+            winText.text = "You Win!";
         }
     }
 }
